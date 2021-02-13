cdfcde380938a5e6deae895e1f8b7eb420662f89
Ahjaen <lettucesnypes@gmail.com>
2021-02-13 11:48:15 -0500

    car track

--- js/Game.js
+++ js/Game.js
@@ -30,9 +30,13 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage(car1Image)
     car2 = createSprite(300,200);
+    car2.addImage(car2Image)
     car3 = createSprite(500,200);
+    car3.addImage(car3Image)
     car4 = createSprite(700,200);
+    car4.addImage(car4Image)
     cars = [car1, car2, car3, car4];
   }
 
@@ -42,13 +46,12 @@ class Game {
     Player.getPlayerInfo();
     
     if(allPlayers !== undefined){
-      //var display_position = 100;
-      
-      //index of the array
+      background("#4A4A4A")
+      image(track, 0, - displayHeight * 4, displayWidth, displayHeight * 5)
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 145;
       var y;
 
       for(var plr in allPlayers){
@@ -78,7 +81,13 @@ class Game {
       player.distance +=10
       player.update();
     }
+    if(player.distance > 3500){
+      gameState = 2
+    }
 
     drawSprites();
+  }
+  end (){
+    console.log("Game Ended")
   }
 }
